207f6d6c9e58046be1ac534ef9b0a063b765a979
Malinda <salgaduwml@gmail.com>
2025-11-17 19:43:25 +0530

    setting up api client

---
+++ mobile/utils/api.ts
@@ -0,0 +1,35 @@
+import axios, { AxiosInstance } from "axios";
+import { useAuth } from "@clerk/clerk-expo";
+
+const API_BASE_URL = "https://x-clone-rn-bay-two.vercel.app/api";
+
+export const createApiClient = (
+  getToken: () => Promise<string | null>
+): AxiosInstance => {
+  const api = axios.create({
+    baseURL: API_BASE_URL,
+  });
+
+  api.interceptors.request.use(async (config) => {
+    const token = await getToken();
+    console.log("TOKEN FROM CLERK:", token);
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  return api;
+};
+
+export const useApiClient = (): AxiosInstance => {
+  const { getToken } = useAuth();
+  return createApiClient(getToken);
+};
+
+export const userApi = {
+  syncUser: (api: AxiosInstance) => api.post(`/users/sync`),
+  getCurrentUser: (api: AxiosInstance) => api.get(`/users/me`),
+  updateProfile: (api: AxiosInstance, data: any) =>
+    api.put(`/users/profile`, data),
+};
